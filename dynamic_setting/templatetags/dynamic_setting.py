e06fd22fb1b7309129fb6f2a87a166517c867eab
Karl Castillo <kcastillo@mstar.io>
2014-12-02 09:37:05 -0800

    added template tag to get setting data in the template

---
+++ dynamic_setting/templatetags/dynamic_setting.py
@@ -0,0 +1,10 @@
+from django import template
+from dynamic_setting.models import Setting
+
+register = template.Library()
+
+@register.simple_tag
+def get_setting(setting_name):
+    """Gets the data stored inside the setting."""
+    setting = Setting.objects.setting(setting_name)
+    return setting.data
